cb1c4712a07e90909d3db937278d94a1dd77a003
chenc <zhaol-a@ubuntu.ubuntu-domain>
2013-09-15 20:03:41 +0800

    diatom_ooze

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiatomOoze::Application.config.secret_token = '8893665e4770a27526196f5b5a35a947124487e3dd0e53edf8733719d2b92b0217c9673ae7d8b026e202ad8b9292d250d1d890490d3fa3ff985e69768b3271ae'
